24a1c73d0ab190c8c9f593388ef086150bc6dd15
Miguel Muñoz Serafín <mmunoz@dotnetpuebla.com>
2024-08-31 18:32:27 -0600

    Fixed after HomeController

--- src/after/DroneDelivery-after/Controllers/HomeController.cs
+++ src/after/DroneDelivery-after/Controllers/HomeController.cs
@@ -42,7 +42,17 @@
             stopWatch.Start();
 
             var httpClient = httpClientFactory.CreateClient();
-            var urlBuilder = new UriBuilder(this.Request.Scheme, this.Request.Host.Host, Request.Host.Port.Value);
+
+            UriBuilder urlBuilder;
+            if (Request.Host.Port.HasValue)
+            {
+                urlBuilder = new UriBuilder(this.Request.Scheme, this.Request.Host.Host, Request.Host.Port.Value);
+            }
+            else
+            {
+                urlBuilder = new UriBuilder(this.Request.Scheme, this.Request.Host.Host);
+            }
+
             httpClient.BaseAddress = urlBuilder.Uri;
 
             var tasks = new Task<HttpResponseMessage>[RequestCount];
