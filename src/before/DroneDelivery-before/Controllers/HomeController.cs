430e395a558d4574295d9bded27b563bbcd68787
Miguel Muñoz Serafín <mmunoz@dotnetpuebla.com>
2024-08-31 18:20:05 -0600

    added messages to homecontroller

--- src/before/DroneDelivery-before/Controllers/HomeController.cs
+++ src/before/DroneDelivery-before/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Net.Http.Json;
+using System.Text;
 
 namespace DroneDelivery_before.Controllers
 {
@@ -42,38 +43,42 @@ namespace DroneDelivery_before.Controllers
         [Route("/[controller]/SendRequests")]
         public IActionResult SendRequests()
         {
+            var Sb = new StringBuilder();
             try
             {
                 var stopWatch = new Stopwatch();
                 stopWatch.Start();
-
+                Sb.AppendLine("After stopWatch");
                 var httpClient = httpClientFactory.CreateClient();
+                Sb.AppendLine("After CreateHttpClient");
                 UriBuilder urlBuilder;
                 if (Request.Host.HasValue)
                     urlBuilder = new UriBuilder(this.Request.Scheme, this.Request.Host.Host, Request.Host.Port.Value);
                 else
                     urlBuilder = new UriBuilder(this.Request.Scheme, this.Request.Host.Host);
-
+                Sb.AppendLine("After setting urlBuilder");
                 httpClient.BaseAddress = urlBuilder.Uri;
 
                 var tasks = new Task<HttpResponseMessage>[RequestCount];
-
+                Sb.AppendLine("Before for");
                 for (int i = 0; i < RequestCount; i++)
                 {
                     tasks[i] = httpClient.PostAsJsonAsync("/api/DeliveryRequests", payload);
                 }
-
+                Sb.AppendLine("After for");
                 Task.WaitAll(tasks);
-
+                Sb.AppendLine("After WaitAll");
                 int Errors = tasks.AsParallel()
                     .Count(t => t.Result.StatusCode != System.Net.HttpStatusCode.Created);
-
+                Sb.AppendLine("After count errors");
                 stopWatch.Stop();
                 ViewBag.Message = $"{RequestCount} messages sent in {stopWatch.Elapsed.Seconds} seconds  with {Errors} errors";
             }
             catch (Exception ex)
             {
-                ViewBag.Message = ex.ToString();
+                Sb.AppendLine(ex.ToString());
+                ViewBag.Message = Sb.ToString();
+
             }
             return View();
         }
